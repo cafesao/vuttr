6c2f79b5550f6e41775f37898197f7b4a7b81588
cafesao <gabriel.diasdutra2@gmail.com>
2020-05-08 22:06:57 -0300

    feat: Adicionando a conexão e as funções do DB
    O arquivo JS connection, faz a conexão com o banco de dados, e o arquivo dbFunctions, expõe as funcionalidades do banco de dados, ou seja, as Querys.

---
+++ src/db/connection.js
@@ -0,0 +1,11 @@
+const { Client } = require('pg')
+
+const client = new Client({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+})
+
+module.exports = client
---
+++ src/db/dbFunctions.js
@@ -0,0 +1,51 @@
+const client = require('../db/connection')
+
+client.connect()
+
+module.exports = {
+  createTable: () => {
+    client.query(
+      `
+      CREATE TABLE IF NOT EXISTS tools(
+        id SERIAL PRIMARY KEY,
+        title VARCHAR NOT NULL UNIQUE,
+        link VARCHAR NOT NULL,
+        description VARCHAR NOT NULL,
+        tags text []
+      )
+      `,
+    )
+  },
+  queryTools: async () => {
+    return await client.query(`SELECT * FROM tools`)
+  },
+  queryToolsSelect: async (tag) => {
+    return await client.query(
+      `
+        SELECT *
+        FROM tools
+        WHERE tags @> ARRAY[$1]::text[]
+      `,
+      [tag],
+    )
+  },
+  createTools: async (title, link, description, tags) => {
+    return client.query(
+      `
+      INSERT INTO tools (title, link, description, tags)
+        VALUES 
+        ($1, $2, $3, $4)
+      RETURNING *
+      `,
+      [title, link, description, tags],
+    )
+  },
+  deleteTools: async (id) => {
+    return client.query(
+      `
+      DELETE FROM tools where id=$1
+      `,
+      [id],
+    )
+  },
+}
