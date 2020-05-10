ddf89ada5e40e72caa8a98b91f99beb22764bc50
cafesao <gabriel.diasdutra2@gmail.com>
2020-05-10 20:36:45 -0300

    feat: Modificando as rotas e os controller para o passport

--- src/controllers/vuttrControllers.js
+++ src/controllers/vuttrControllers.js
@@ -1,9 +1,22 @@
-const dbFunctions = require('../db/dbFunctions')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const dbFunctionsTools = require('../db/dbFunctionsTools')
+const dbFunctionsAccounts = require('../db/dbFunctionsAccounts')
+const blacklist = require('../redis/blacklistController')
+
+function criaTokenJWT(user) {
+  const payload = {
+    username: user.rows[0].username,
+  }
+  const token = jwt.sign(payload, process.env.CHAVE_JWT, { expiresIn: '15m' })
+  return token
+}
 
 module.exports = {
   allTools: async (req, res) => {
     try {
-      const tools = await dbFunctions.queryTools()
+      const tools = await dbFunctionsTools.queryTools()
       if (tools.rows.length > 0) {
         res.json(tools.rows)
       } else {
@@ -18,7 +31,7 @@ module.exports = {
   selectTools: async (req, res) => {
     const tag = req.query.tag
     try {
-      const tools = await dbFunctions.queryToolsSelect(tag)
+      const tools = await dbFunctionsTools.queryToolsSelect(tag)
       if (tools.rows.length > 0) {
         res.json(tools.rows)
       } else {
@@ -30,10 +43,39 @@ module.exports = {
       res.status(500).json({ resposta: erro.detail })
     }
   },
+  register: async (req, res) => {
+    const { username, password } = req.body
+    try {
+      const passwordHash = await bcrypt.hash(password, 12)
+      await dbFunctionsAccounts.createAccount(username, passwordHash)
+      res.status(201).send()
+    } catch (error) {
+      res.status(500).json({ erro: error.message })
+    }
+  },
+  login: async (req, res) => {
+    const token = criaTokenJWT(req.user)
+    res.set('Authorization', token)
+    res.status(204).send()
+  },
+  logout: async (req, res) => {
+    try {
+      const token = req.info
+      await blacklist.add(token)
+      res.status(204).send()
+    } catch (error) {
+      res.status(500).json(error)
+    }
+  },
   createTools: async (req, res) => {
     const { title, link, description, tags } = req.body
     try {
-      const tool = await dbFunctions.createTools(title, link, description, tags)
+      const tool = await dbFunctionsTools.createTools(
+        title,
+        link,
+        description,
+        tags,
+      )
       res.json(tool.rows[0])
     } catch (erro) {
       if (erro.code == 23505) {
@@ -52,7 +94,7 @@ module.exports = {
   deleteTools: async (req, res) => {
     const id = req.params.id
     try {
-      const tool = await dbFunctions.deleteTools(id)
+      const tool = await dbFunctionsTools.deleteTools(id)
 
       if (tool.rowCount > 0) {
         res.status(204).send()
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,10 +2,14 @@ const express = require('express')
 
 const routes = express.Router()
 const vuttrControllers = require('../controllers/vuttrControllers')
+const middlewareAuth = require('../auth/middlewareAuth')
 
 routes.get('/allTools', vuttrControllers.allTools)
 routes.get('/tools', vuttrControllers.selectTools)
-routes.post('/tools', vuttrControllers.createTools)
-routes.delete('/tools/:id', vuttrControllers.deleteTools)
+routes.post('/cadastro', vuttrControllers.register)
+routes.post('/login', middlewareAuth.local, vuttrControllers.login)
+routes.post('/logout', middlewareAuth.bearer, vuttrControllers.logout)
+routes.post('/tools', middlewareAuth.bearer, vuttrControllers.createTools)
+routes.delete('/tools/:id', middlewareAuth.bearer, vuttrControllers.deleteTools)
 
 module.exports = routes
