2e3ce567b3f123ee0a8c547bcbd0cc05b6728bd8
cafesao <gabriel.diasdutra2@gmail.com>
2020-05-10 20:34:54 -0300

    feat: Adicionando arquivos para o Redis
    blacklist.js : Para criar o cliente no redis.
    blacklistController.js : Para controlar as ações dentro do redis.

---
+++ src/redis/blacklistController.js
@@ -0,0 +1,26 @@
+const blacklist = require('./blacklist')
+
+const { promisify } = require('util')
+const existsAsync = promisify(blacklist.exists).bind(blacklist)
+const setAsync = promisify(blacklist.set).bind(blacklist)
+
+const jwt = require('jsonwebtoken')
+const { createHash } = require('crypto')
+
+function geraTokenHash(token) {
+  return createHash('sha256').update(token).digest('hex')
+}
+
+module.exports = {
+  add: async (token) => {
+    const dataExpiracao = jwt.decode(token).exp
+    const tokenHash = geraTokenHash(token)
+    await setAsync(tokenHash, '')
+    blacklist.expireat(tokenHash, dataExpiracao)
+  },
+  hasToken: async (token) => {
+    const tokenHash = geraTokenHash(token)
+    const resultado = await existsAsync(tokenHash)
+    return resultado === 1
+  },
+}
