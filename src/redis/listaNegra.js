0393003ce1d6b0be623e11348c8b9ad51aa683e4
cafesao <gabriel.diasdutra2@gmail.com>
2020-05-12 02:55:09 -0300

    refactor: Pequenas modificações

--- src/redis/listaNegra.js
+++ src/redis/listaNegra.js
@@ -1,5 +1,8 @@
 const redis = require('redis')
 
+console.log('[Redis] Iniciando server Redis...')
 module.exports = redis.createClient({
+  host: 'redis-server_db',
+  port: 6379,
   prefix: 'listaNegra:',
 })
